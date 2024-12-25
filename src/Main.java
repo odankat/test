7586bc91dcad5d8bc58e85a62ec295d02c7c8307
HomeMedia <taknado13@yandex.ru>
2024-12-25 22:34:37 +0700

    test

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(12, "Антон"));
+        users.add(new User(15, "Ян"));
+        users.add(new User(55, "Максим"));
+        users.add(new User(19, "Инокентий"));
+        users.add(new User(32, "Василия"));
+        users.add(new User(32, "Василия"));
+        User user = new User(32, "Василия");
+        users.add(user);
+        users.add(user);
+        users.add(user);
+        users.add(user);
+
+
+        System.out.println(User.old(users));
+
+
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class User {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public User(int age, String name) {
+        this.name = name;
+        this.age = age;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    public String toString() {
+        return name + " " + age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+//    public boolean equals(Objects o) {
+//        if( this == o ) return true;
+//        if(o == null || getClass() != o.getClass()) return fals
+//
+//    }
+
+    public static User old(Collection<User> users) {
+        HashSet<User> qwe = new HashSet<>(users);
+        System.out.println(users);
+        users.clear();
+        users.addAll(qwe);
+        System.out.println(qwe);
+        ArrayList<User> set = users.stream().distinct().sorted(Comparator.comparing(User::getAge).
+                thenComparing(User::getName)).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(set);
+        return set.stream().max(Comparator.comparing(User::getAge)).orElse(null);
+    }
+
+
+}
